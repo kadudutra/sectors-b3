37fc3bde1827d410bb3820ca7d379eed5f13c65f
Kadu Dutra <carlos.rocha@edu.unipar.br>
2020-05-02 11:57:24 -0300

    subsectors

---
+++ Subsectors/src/Subsectors.php
@@ -0,0 +1,86 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Subsectors;
+
+/**
+ * Subsectors B3
+ *
+ * @copyright (c) 2019.
+ * @link <https://github.com/kadudutra/sectors-b3.git>
+ * @author Kadu Dutra <kadudutra7@gmail.com>
+ */
+class Subsectors
+{
+    const BENS_INDUSTRIAIS = [
+        'Comércio',
+        'Construção e Engenharia',
+        'Máquinas e Equipamentos',
+        'Material de Transporte',
+        'Serviços',
+        'Transporte'
+    ];
+
+    const COMUNICACOES = [
+        'Mídia',
+        'Telecomunicações',
+        'Telefonia Fixa',
+    ];
+
+    const CONSUMO_CICLICO = [
+        'Automóveis e Motocicletas',
+        'Comércio',
+        'Construção Civil',
+        'Diversos',
+        'Hoteis e Restaurantes',
+        'Tecidos. Vestuário e Calçados',
+    ];
+
+    const CONSUMO_NAO_CICLICO = [
+        'Agropecuária',
+        'Alimentos Processados',
+        'Bebidas',
+        'Comércio e Distribuição',
+        'Produtos de Uso Pessoal e de Limpeza',
+    ];
+
+    const FINANCEIRO = [
+        'Exploração de Imóveis',
+        'Holdings Diversificadas',
+        'Intermediários Financeiros',
+        'Previdência e Seguros',
+        'Securitizadoras de Recebíveis',
+    ];
+
+    const MATERIAIS_BASICOS = [
+        'Embalagens',
+        'Madeira e Papel',
+        'Materiais Diversos',
+        'Mineração',
+        'Químicos',
+        'Siderurgia e Metalurgia',
+    ];
+
+    const PETROLEO_GAS_BIOCOMBUSTIVEIS = [
+        'Petróleo. Gás e Biocombustíveis',
+    ];
+
+    const SAUDE = [
+        'Comércio e Distribuição',
+        'Equipamentos',
+        'Medicamentos e Outros Produtos',
+        'Serv.Méd.Hospit..Análises e Diagnósticos',
+    ];
+
+    const TECNOLOGIA_DA_INFORMACAO = [
+        'Computadores e Equipamentos',
+        'Programas e Serviços',
+    ];
+
+    const UTILIDADE_PUBLICA = [
+        'Água e Saneamento',
+        'Energia Elétrica',
+        'Gás',
+    ];
+}
